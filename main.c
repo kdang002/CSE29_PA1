6b57327789aa253167e6398397468b6740b512c7
kdang002 <kid002@ucsd.edu>
2024-10-09 17:04:03 +0000

    Working on MS3

--- main.c
+++ main.c
@@ -43,7 +43,7 @@ int32_t capitalize_ascii(char str[])
 // @param: a char 
 // @return: width of how bytes
 //
-int32_t width_from_start_bytes(char start_byte)
+int32_t width_from_start_bytes(unsigned char start_byte)
 {
     //For ASCII, isolate first bit and check start byte is 0b0xxxxxxx
     if ((start_byte & 0b10000000) == 0b00000000) 
@@ -69,7 +69,14 @@ int32_t width_from_start_bytes(char start_byte)
 
     //Continuation bytes or multi bytes will always start with 0b10xxxxxx
     else if ((start_byte & 0b11000000) == 0b10000000)
+    {
+
         return -1;
+    }
+
+    return 0;
+    
+
 }
 
 // This function will return the length of the UTF-8 string CORRECTLY
@@ -97,32 +104,33 @@ int32_t utf8_strlen(char str[])
 //
 int32_t codepoint_index_to_byte_index(char str[], int32_t cpi)
 {
+    
     int codePointIterator = 0;
     int byteIterator = 0;
 
     while (str[byteIterator] != '\0')
     {
+        int width = width_from_start_bytes((unsigned char)str[byteIterator]);
+        if (width == -1)
+            return -1;
+
+       byteIterator += width;
+       codePointIterator++;
 
         if (codePointIterator == cpi)
         {
             return byteIterator;
         }
-
-        int width = width_from_start_bytes(str[byteIterator]);
-        if (width == -1)
-            return -1;
-
-        byteIterator += width;
-        codePointIterator++;
     }
 
-    return -1;
+    return 0;
+    
 }
 
 // This function will convert codepoint_index_to_byte_index
 // @param: UTF-8 string, codepoint index (cpi)
 // @return: int byte index
-// FUNCTIONING PARTIALLY DUE TO MEMORY LEAK
+// FUNCTIONING NOW - DUE TO codepoint_index_to_byte_index()
 void utf8_substring(char str[], int32_t cpi_start, int32_t cpi_end, char result[])
 {
     int byteStart = codepoint_index_to_byte_index(str, cpi_start);
@@ -130,15 +138,19 @@ void utf8_substring(char str[], int32_t cpi_start, int32_t cpi_end, char result[
 
     printf("BYTE START/END: %d %d \n", byteStart, byteEnd);
 
-    if (byteStart == -1 || byteEnd == -1 || byteStart > byteEnd)
+    if (byteStart == -1 || byteEnd == -1)
     {
         return;
     }
 
+    if (cpi_start < 0 || cpi_end <= cpi_start)
+        return;
+
     int index = 0;
 
     //This loop will copy the indicated bytes into result[]
-    for (int i = byteStart; i <= byteEnd; i++)
+    ///LOGIC ERROR: MEMORY CONTROLS
+    for (int i = byteStart; i < byteEnd; i++)
     {
         result[index] = str[i];
         index++;
@@ -148,7 +160,8 @@ void utf8_substring(char str[], int32_t cpi_start, int32_t cpi_end, char result[
 }
 
 
-// Takes a UTF-8 encoded string and a codepoint index, and returns a decimal representing the codepoint at that index.
+// Takes a UTF-8 encoded string and a codepoint index, 
+// and returns a decimal representing the codepoint at that index.
 // @param: string, codepointindex
 // @return: decimal represent codepoint at index.
 int32_t codepoint_at(char str[], int32_t cpi)
@@ -158,38 +171,54 @@ int32_t codepoint_at(char str[], int32_t cpi)
 
     while (str[byteIterator] != '\0')
     {
-        int width = width_from_start_bytes(str[byteIterator]);
-        
-        if (codePointIterator == cpi)
-        {
-            //CHECK FOR THE CASE WHEN CPI is UTF, DOESN'T RETURN -1
-            if (str[byteIterator] >= 0)
-                return str[byteIterator]; // SHOULD RETURN AT BYTE ITERATOR TO PREVENT WRONG MEMORY.
-
-            else
-                return -1;
-        }
-
+        int width = width_from_start_bytes((unsigned char)str[byteIterator]);
         if (width == -1)
             return -1;
 
         byteIterator += width;
         codePointIterator++;
+
+        if (codePointIterator == cpi)
+        {
+            //CHECK FOR THE CASE WHEN CPI is UTF, DOESN'T RETURN -1
+            return str[byteIterator]; // SHOULD RETURN AT BYTE ITERATOR TO PREVENT WRONG MEMORY.
+        }
     }
-        return 0;
+
+    return 0;
 }
 
-char is_animal_emoji_at(char str[], int32_t cpi)
+// Takes a UTF-8 encoded string and an codepoint index, and returns if the code point at that index is an animal emoji.
+// @param: UTF-8 string, codePointIndex
+// @return: 1 : TRUE, 0 : FALSE
+// NOT WORKING
+int is_animal_emoji_at(char str[], int32_t cpi)
 {
-    
+    int codePoint = codepoint_at(str, cpi);
+
+    ///LOGIC ERROR: THE CONDITION NEVER GETS EXECUTED
+    return ((codePoint >= 0b00011111010000000000000000000000 && codePoint <= 0b00011111010000111111) ||  // NESTED LIST OF EMOJI BASE ON
+        (codePoint >= 0b00011111001001100000 && codePoint <= 0b00011111001010111110))   ;                // WIKI'S UNICODE
+
 }
 
 int main()
 {
-    char str[] = "Joséph";
-    int32_t idx = 3;
-    //printf("Codepoint index %d is byte index %d\n", idx, codepoint_index_to_byte_index("Joséph", idx));
-    printf("Codepoint at %d in %s is %d\n", idx, str, codepoint_at(str, idx)); // 'p' is the 4th codepoint
+    //substring() test
+    char result[17];
+    char newS[] = "🦀🦮🦮🦀🦀🦮🦮";
+    utf8_substring(newS, 3, 7, result);
+    printf("String: %s\nSubstring: %s\n", newS, result); // these emoji are 4 bytes long
+
+    //animal_emoji() test
+    char str[] = "🐀Rat";
+    int32_t idx = 0;
+    printf("Codepoint index %d of %s is ANIMAL: %d\n", idx, str, is_animal_emoji_at(str, idx));
+
+    //codepoint_at() test
+    char str1[] = "Joséph";
+    idx = 1;
+    printf("Codepoint at %d in %s is %d\n", idx, str1, codepoint_at(str1, idx)); // 'p' is the 4th codepoint
 
     return 0;
 }
